bfdc74e051985886e71252bc250787a655a78e32
Gus - MVC <gustavotiseira@gmail>
2024-03-20 11:56:04 -0300

    Clase 215 Instalacion Socket.io

--- models/server.js
+++ models/server.js
@@ -3,7 +3,9 @@ const cors = require('cors')
 class Server {
     constructor(){
     this.app = express()
-    this.port = process.env.PORT    
+    this.port = process.env.PORT
+    this.server = require('http').createServer(this.app)
+    this.io = require('socket.io')(this.server)
     this.path = {}    
 
         //Middlewares
@@ -26,7 +28,7 @@ class Server {
     }
 
     listen(){
-        this.app.listen(this.port,()=>{console.log(`Servidor corriendo en el puerto ${this.port}`);})
+        this.server.listen(this.port,()=>{console.log(`Servidor corriendo en el puerto ${this.port}`);})
     }
 }
 
