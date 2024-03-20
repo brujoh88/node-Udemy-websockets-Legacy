ef38877e6be527d9d28ec62aa7228637e72d1a9c
Gus - MVC <gustavotiseira@gmail>
2024-03-20 12:27:51 -0300

    Clase 218 emitir desde el cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,5 +1,7 @@
-const lblOnline = document.querySelector('#lblOnline')
+const btnEnviar = document.querySelector('#btnEnviar')
 const lblOffline = document.querySelector('#lblOffline')
+const lblOnline = document.querySelector('#lblOnline')
+const txtMensaje = document.querySelector('#txtMensaje')
 
 const socket = io()
 
@@ -15,4 +17,14 @@ socket.on('disconnect',()=>{
     console.log('Desconectado del servidor');
     lblOffline.style.display = ''
     lblOnline.style.display = 'none'
+})
+
+btnEnviar.addEventListener('click', ()=>{
+    const mensaje = txtMensaje.value
+    const payload = {
+        mensaje,
+        id: '123asd',
+        fecha: new Date().getTime()
+    }
+    socket.emit('enviar-mensaje', payload)
 })
